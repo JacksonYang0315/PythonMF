5b7230f8e68906c721b4c7e956702ff65a8a39d5
Jackson Yang <jacksony@gmail.com>
2020-04-22 22:51:15 +0800

    Update jcl.py

--- jcl.py
+++ jcl.py
@@ -1,5 +1,24 @@
 import re
-with open("C:/Users/ChiaJuiYang/Desktop/Apply_Check.txt", "r") as f:
-    for line in f.readlines():
-        line = line.strip('\n')
-        print(line)
+f = open("C:/Users/ChiaJuiYang/Desktop/Apply_Check.txt","r")
+lines = f.readlines()
+f.close()
+hold_action_list = []
+ptf_applied_list = []
+
+for i,line in enumerate(lines):
+    if line.find("SYSMODS APPLIED") != -1:
+        ptf_applied_count = int(line[79:133].strip())
+    if line.find("TYPE    REASON ID  FMID     SYSMOD   STATUS    ++HOLD DATA") != -1:
+        hold_action_list.append(i)
+        # print(max(hold_action_list))
+    if line.find("TYPE    REASON ID  REPORT      SYSMODS AFFECTED") != -1:
+        ptf_applied_list.append(i)
+        # print(max(ptf_applied_list))
+
+# print(min(hold_action_list))
+# print(max(hold_action_list))
+# print(min(ptf_applied_list))
+data=open("C:/Users/ChiaJuiYang/Desktop/abc.txt",'w+')
+for i in range(min(hold_action_list),min(ptf_applied_list)):
+    print(lines[i].strip('\n'),file=data)
+data.close()
